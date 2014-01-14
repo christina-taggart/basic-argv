9f876ed632c662376efc1f19ae5a417e2a9dba45
Apprentice <apprentice@dbc27.local>
2014-01-14 10:31:59 -0800

    added ARGV to pig latin method

---
+++ pig_latin.rb
@@ -0,0 +1,15 @@
+def convert_word_to_pig_latin(word)
+  return "#{word} "  if word.match(/^[aeiouy]/i)
+  consonants = word.match(/^[^aeiouy]+/i).to_s
+  "#{word.gsub(consonants, "")}#{consonants}ay "
+end
+
+def convert_sentence_to_pig_latin
+
+  p ARGV.map { |word| convert_word_to_pig_latin(word) }.join("")
+end
+
+convert_sentence_to_pig_latin
+
+
+
