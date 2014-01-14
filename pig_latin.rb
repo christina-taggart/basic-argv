e18bbd0488e0c72f6e1fdf1848531e456099345e
John Olmsted <null>
2014-01-14 10:21:00 -0800

    ARGVifies pig_latin

---
+++ pig_latin.rb
@@ -0,0 +1,16 @@
+def pig_latin(english)
+  english.downcase!
+  return english if english == "" || "aeiouy".include?(english[0])
+  /^(?<first_consonants>[^aeiouy\d\W_]+)(?<rest_of_word>.*)/ =~ english
+  rest_of_word + first_consonants + "ay"
+end
+
+def pig_latin_sentence(sentence)
+  sentence = sentence.split(" ")
+  sentence.map! { |word| pig_latin(word) }
+  sentence.join(" ")
+end
+
+#-----ARGV with pig latin)
+
+puts pig_latin_sentence(ARGV.join(" "))
