faf49d5fa7f82efa0c2a3eb444180fd5b1c6cc88
& Nicholas Cu <pair+hzla00.nicholascu@devbootcamp.com>
2014-01-14 10:34:02 -0800

    Release 0 and Release 1

---
+++ rpn.rb
@@ -0,0 +1,19 @@
+rpn = ARGV.join(" ")
+
+
+  def evaluate(rpn)
+    rpn_array = rpn.split(" ")
+    rpn_output = []
+    rpn_array.each do |x|
+      if  x == "+" or x == "-" or x == "*"
+        new_value = rpn_output[-2].send(x, rpn_output[-1])
+        rpn_output.pop(2)
+        rpn_output << new_value
+      else (0..9).include?(x.to_i)
+        rpn_output << x.to_i
+      end
+    end
+    rpn_output[0]
+  end
+
+ p evaluate(rpn)
